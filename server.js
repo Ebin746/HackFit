f1c39129880093f801879eda8fb0c24524d351d9
Ebin746 <146419456+Ebin746@users.noreply.github.com>
2024-08-18 22:26:58 +0530

    third commits

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
 const express=require("express");
 const databaseConnection = require("./DB/database");
 const app=express();
 const userRouter=require("./routers/users");
-
+const notifyRouter=require("./routers/notify");
+const animalRouter=require("./routers/animal");
 app.use(express.json())
+app.use(express.static("public"));
 app.use("/user",userRouter);
-
+app.use("/notify",notifyRouter)
+app.use("/animals",animalRouter)
 app.listen(3000,()=>{
     console.log("server started on the Port",3000)
     databaseConnection()
