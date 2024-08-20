4cb50d5d558817c6f7aeeef895a7daed90771dba
Damian antony <jemcydami55@gmail.com>
2024-08-20 12:56:49 +0530

    "feat:added new features"

--- models/user.js
+++ models/user.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: { type: String},
+    name: { type: String },
     password: { type: String },
-    phoneNumber: { type: String},
+    phoneNumber: { type: String },
     location: {
         latitude: { type: Number },
         longitude: { type: Number }
-    }
+    },
+     comments: { type: String},
+     voteType: { type: String, enum: ['upvote', 'downvote'] }
+
 });
 
 module.exports = mongoose.model("User", userSchema);
--- utils/sms.js
+++ utils/sms.js
@@ -1,39 +1,37 @@
 const twilio=require("twilio");
-// const client=twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
+const client=twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
 
 
-//const sendSms=()=>{
-//console.log("sms ")
-//}
-//const sendSms=async(messageBody,recipientPhoneNumber)=>{
-// try {
+
+const sendSms=async(messageBody,recipientPhoneNumber)=>{
+try {
     
-    // const message = await client.messages.create({
-    //     body: messageBody,
-    //     from: process.env.PHONE_NUMBER,  // Twilio phone number
-    //     to: "+919747859313" // Recipient's phone number
-    // });
+    const message = await client.messages.create({
+        body: messageBody,
+        from: process.env.PHONE_NUMBER,  // Twilio phone number
+        to: "+919747859313" // Recipient's phone number
+    });
 
-    // res.status(200).json({message:"sms send to your mobile number"});
-   // console.log("message send to phone number");
-// } catch (error) {
-//     console.log(error);
-// }
-// }
-//module.exports={sendSms};
+    res.status(200).json({message:"sms send to your mobile number"});
+   console.log("message send to phone number");
+} catch (error) {
+    console.log(error);
+}
+}
+module.exports={sendSms};
 
 
-const sendSms = (messageBody, recipientPhoneNumber) => {
-    // Simulate sending SMS
-    console.log(`Simulated SMS sent:`);
-    console.log(`To: ${recipientPhoneNumber}`);
-    console.log(`Message: ${messageBody}`);
+// const sendSms = (messageBody, recipientPhoneNumber) => {
+//     // Simulate sending SMS
+//     console.log(`Simulated SMS sent:`);
+//     console.log(`To: ${recipientPhoneNumber}`);
+//     console.log(`Message: ${messageBody}`);
 
-    // Simulate a delay
-    setTimeout(() => {
-        console.log("Message successfully sent (simulated).");
-    }, 1000); // Delay in milliseconds
-};
+//     // Simulate a delay
+//     setTimeout(() => {
+//         console.log("Message successfully sent (simulated).");
+//     }, 1000); // Delay in milliseconds
+// };
 
-// Export the function
-module.exports = { sendSms };
+// // Export the function
+ //module.exports = { sendSms };
